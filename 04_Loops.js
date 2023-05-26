85355eac0a7b9a909b4d604b0ae2a64dfb68f495
nevin <aydingezernevin@gmail.com>
2023-05-26 15:51:12 -0400

    Arrays are introduced

--- 04_Loops.js
+++ 04_Loops.js
@@ -11,3 +11,14 @@ for (let i = 0; i < 5 ; i++) {
         
     }
     console.log(j); //this type is global and function scope
+
+    let n1=0;
+    while (n1<3) {
+        console.log(n1);
+        n1++;
+    }
+    let n2=0;
+    do {
+        console.log(n2);
+        n2++
+    } while (n2<5);
